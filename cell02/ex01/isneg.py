c1c40026d4a0075621d50071101a430192c5b3d8
Nachigoru <nachdhaphonc@gmail.com>
2026-04-25 04:08:35 +0000

    isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+number = input().strip()
+number = int(number)
+
+if number > 0:
+    print("This number is positive.")
+elif number < 0:
+    print("This number is negative.")
+else:
+    print("Error pls try again")
