0da5a1217b6416144321fd436c31e9b5008a5d1f
Nachigoru <nachdhaphonc@gmail.com>
2026-04-25 05:21:22 +0000

    advance_mult.py

---
+++ cell03/ex03/advance_mult.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    table = 0
+
+    while table <= 10:
+        print(f"Table de {table}:", end=" ")
+
+        i = 0
+        while i <= 10:
+            print(table * i, end=" ")
+            i += 1
+
+        print()
+        table += 1
